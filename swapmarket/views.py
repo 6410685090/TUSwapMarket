718e7cf916d8d0bd7cd92020ee43f2ee60a8815a
Jedtapat Tanthiew <6410685090@cn330>
2023-11-04 21:08:01 +0700

    add about page

--- swapmarket/views.py
+++ swapmarket/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 
 # Create your views here.
+
+
+def about(request):
+    return render(request,"swapmarket/about.html")
