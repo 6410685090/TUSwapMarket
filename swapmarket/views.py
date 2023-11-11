b790f0eb0c05fa0438b92fd22a8341ff4986dc3e
Jedtapat Tanthiew <6410685090@cn330>
2023-11-12 00:09:11 +0700

    add item in views

--- swapmarket/views.py
+++ swapmarket/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render , redirect
-
+from .models import Item
 # Create your views here.
 
 def home(request):
@@ -7,8 +7,12 @@ def home(request):
         if request.user.is_staff:
             return redirect('/admin/')
         else:
-            return render(request, 'user/homepage.html')        
-    return render(request, 'swapmarket/homepage.html')
+            return render(request, 'user/homepage.html',{
+            'item' : Item.objects.all()
+        })  
+    return render(request, 'swapmarket/homepage.html',{
+        'item' : Item.objects.all()
+    })
 
 def about(request):
     if request.user.is_authenticated:
